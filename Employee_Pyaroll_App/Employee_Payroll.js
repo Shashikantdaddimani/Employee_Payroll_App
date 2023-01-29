0052a3d9cefefa7192e17b14cb665de7c6c6fcf7
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2023-01-29 23:42:12 +0530

    Added:Logic for Create Employee payroll Object

--- Employee_Pyaroll_App/Employee_Payroll.js
+++ Employee_Pyaroll_App/Employee_Payroll.js
@@ -83,3 +83,48 @@ window.addEventListener("DOMContentLoaded", (event) => {
         output.textContent = salary.value;
     });
 });
+const save = () => {
+    try {
+        let employeePayrollData = createEmployeePayroll();
+    }
+    catch (exception) {
+        return;
+    }
+}
+const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeepayrollData();
+    try {
+        employeePayrollData.name = getInputValueById("#name");
+    }
+    catch (exception) {
+        throw exception;
+    }
+    employeePayrollData.profilePic = getSelectedValues("[name=profile]").pop();
+    employeePayrollData.gender = getSelectedValues("[name=gender]").pop();
+    employeePayrollData.department = getSelectedValues("[name=department]");
+    employeePayrollData.salary = getInputValueById("#salary");
+    employeePayrollData.note = getInputValueById("#notes");
+    let date = getInputValueById("#day") + " " + getInputValueById("#month") + " " + getInputValueById("#year");
+    //employeePayrollData.startDate = Date.parse(date);
+    employeePayrollData.startDate = date;
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+}
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let selItems = [];
+    allItems.forEach(item => {
+        if (item.checked) {
+            selItems.push(item.value);
+        }
+    });
+    return selItems;
+}
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+const getInputValue = (id) => {
+    let value = document.getElementById(id).value;
+    return value;
+}
