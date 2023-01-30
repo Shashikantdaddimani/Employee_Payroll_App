8bd945379f0b1e38a35fc779889c2116fa21e6e8
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2023-01-31 00:33:08 +0530

    Added:Logic for Display Employee Details in Tabular Formate using Template Literals

--- Employee-payroll/Employee2.js
+++ Employee-payroll/Employee2.js
@@ -139,3 +139,28 @@ const getInputValue = (id) => {
     let value = document.getElementById(id).value;
     return value;
 }
+
+window.addEventListener('DOMContentLoaded', (event) =>{
+    createInnerHtml();
+});
+
+const createInnerHtml=()=>{
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th";
+    const innerHtml=`${headerHtml}
+
+    <tr>
+    <td><img class="profile" src="../assets/Profile-Images/Ellipse -2.png", alt=""></td>
+    <td>Narayan Mahadevan</td>
+    <td>Male</td>
+    <td><div class='dept-label'></div><div class='dept-label'>Finance</div></td>
+    <td>300000</td>
+    <td>1 Nov 2020</td>
+    <td>
+    <img name="1" onclick ="remove(this)" src="../assets/Icons/delete-black-18dp.svg" alt="delete">
+    <img name="1" onclick ="update(this)" src="../assets/Icons/create-black-18dp.svg" alt="edit">
+    </td>
+    </tr>
+    `;
+    document.querySelector('#table-display').innerHTML=innerHtml;
+
+}
